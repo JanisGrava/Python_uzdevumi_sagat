438b70e1015becd9092e907ec49b4a325749a9cf
JanisGrava <janis.grava2006@gmail.com>
2023-11-03 14:40:24 +0200

    izdariju 2.uzdevumu

--- 2uzd.py
+++ 2uzd.py
@@ -1,4 +1,22 @@
 """ 
 Iegūt ziņas, izmantojot RSS plūsmu no google.lv.
 
+Kas ir RSS?- specials formāts datu izplatīšanai
+
 """
+
+
+import feedparser
+
+#URL uz RSS plūsmu
+rss_url='https://news.google.com/home?hl=en-LV&gl=LV&ceid=LV:en'
+
+#iegūsim RSS plūsmaS DATUS
+kkk=feedparser.parse(rss_url)
+
+#parāda un jānoformē
+
+for entry in kkk.entries:
+    print("Virsraksts:", entry.title)
+    print("Saite:", entry.link)
+    print('\n')
